64be5848ad8fda743f83d6b84410e26ac28200a4
Rebekah <macrosystem@proton.me>
2023-12-25 00:12:15 +0100

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var generateBtn = document.getElementById('generateBtn');
+
+  generateBtn.addEventListener('click', function () {
+    // Implement logic to fetch and generate username
+  });
+});
